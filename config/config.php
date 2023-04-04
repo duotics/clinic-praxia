d9ae8a994f88eea38189b9360016564b1414325a
Daniel Banegas <dbanegasl@gmail.com>
2023-04-04 16:38:49 -0500

    configs folder and files

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+require('paths.php'); //RUTAS de acceso a lugares del framework
+
+date_default_timezone_set('America/Guayaquil');
+setlocale(LC_ALL, "es_EC");
+
+define("cfg", $cfg = startConfigs("cfg"));
+define("cfgMail", $cfgMail = startConfigs("cfgMail"));
+define("cfgService", $cfgService = startConfigs("cfgService"));
+
+$sys['date'] = date('Y-m-d');
+$sys['datet'] = date('Y-m-d H:i:s');
+$sys['time'] = date('H:i:s');
+define('sys', $sys);
+$dU=$_SESSION['dU'] ?? null;
+define('dU', $dU);
+$_SESSION['conf']['theme'] = $_SESSION['du']['THEME'] ?? $_ENV['SYS_THEME'] ?? "zephyre";
---
+++ config/paths.php
@@ -0,0 +1,40 @@
+<?php
+//Paths server
+define('rootP', $rootP . '/');
+define('rootM', $root . '/');
+define('root', array(
+    '0' => rootM,
+    'a' => rootM . 'assets/',
+    'd' => rootM . 'data/',
+    'c' => rootM . 'components/',
+    'o' => rootM . 'config/',
+    'i' => rootM . 'assets/images',
+    'm' => rootM . 'mods/',
+    'f' => rootM . 'frames/',
+    's' => rootM . 'system/',
+    'p' => rootM . 'api/',
+    'r' => rootM . 'resources/',
+    't' => rootM . 'resources/templates/'
+));
+//Paths url
+define('routeP', $_ENV['APP_URLR']);
+define('routeM', $_ENV['APP_URL']);
+define('route', array(
+    '0' => routeM,
+    'a' => routeM . 'assets/',
+    'd' => routeM . 'data/',
+    'c' => routeM . 'components/',
+    'f' => routeM . 'frames/',
+    'o' => routeM . 'config/',
+    'i' => routeM . 'assets/images/',
+    's' => routeM . 'system/',
+    'n' => routeM . 'node_modules/',
+    'p' => routeM . 'api/',
+    'r' => routeM . 'resources/',
+    't' => routeM . 'resources/templates/'
+));
+
+$_SESSION['urlp'] = $_SESSION['urlc'] ?? null;
+$_SESSION['urlc'] = basename($_SERVER['SCRIPT_FILENAME']); //URL clean Current;
+$urlc = $_SESSION['urlc'];
+$urlp = $_SESSION['urlp'];
