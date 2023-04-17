224ae2f70004e4cf08bfcbb26fe8422f0e49cde0
Daniel Banegas <dbanegasl@gmail.com>
2023-04-17 09:43:56 -0500

    api for use functions helper

---
+++ api/json.helper.php
@@ -0,0 +1,24 @@
+<?php require('../init.php');
+$acc = $_REQUEST['acc'] ?? null;
+$val = $_REQUEST['val'] ?? null;
+$LOG = null;
+$ret = null;
+$vP = FALSE;
+try {
+	if ($acc) {
+		switch ($acc) {
+			case 'FechaToEdad':
+				$ret = calcular_edad($val);
+				$vP = TRUE;
+				break;
+			default;
+				throw new Exception('Action not defined');
+		}
+	} else {
+		throw new Exception('No action defined');
+	}
+} catch (Exception $e) {
+	$LOG = $e->getMessage();
+}
+
+echo json_encode(array("status" => $vP, "ret" => $ret, "log" => $LOG));
